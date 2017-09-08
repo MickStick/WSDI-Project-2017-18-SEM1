34662abc66331a085bb299a4e9418b6b07b46b86
Mikhail R. Shaw <mickstick01@hotmail.com>
2017-09-08 18:29:08 -0500

    added content to index.php,head.php.header.php,DBConfig.php in all projects

---
+++ Individual/Part 2/index.php
@@ -0,0 +1,26 @@
+<?php
+    include "controller/MainController.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php
+        $title = "Home";
+        include "partials/head.php";
+    ?>
+</head>
+
+<header>
+    <?php include "partials/header.php"; ?>
+</header>
+<body>
+    <main>
+        
+    </main>
+</body>
+
+<footer>
+    <?php include "partials/footer.php"; ?>
+</footer>
+</html>
---
+++ Individual/Part 4/partials/header.php
@@ -0,0 +1,10 @@
+<img src="public/static/logo.png" alt="LOGO">
+<h1>Site Name</h1>
+<nav>
+    <ul>
+        <li><a href="">Link</a></li>
+        <li><a href="">Link</a></li>
+        <li><a href="">Link</a></li>
+        <li><a href="">Link</a></li>
+    </ul>
+</nav>
